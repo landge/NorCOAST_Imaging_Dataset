b53b2c69e21f2412de9469c07948de2423f7a9b7
landge <tschellhorn@gmail.com>
2023-05-24 14:38:37 +0200

    Added order to csv

--- code/create_csv.py
+++ code/create_csv.py
@@ -21,6 +21,33 @@ def check_term_in_modality(term):
     else:
         return term
 
+def make_order(term):
+    match term:
+        case 'CT-1':
+            return 1
+        case 'CT-2':
+            return 2
+        case  'CTA':
+            return 3
+        case 'DWI':
+            return 4
+        case 'ADC':
+            return 5
+        case '3D-FLAIR':
+            return 6
+        case '3D-T1':
+            return 7
+        case 'SWI':
+            return 8
+        case 'T1':
+            return 9
+        case 'T2':
+            return 10
+        case 'T2-FLAIR':
+            return 11
+        case 'MRA':
+            return 12
+        
 def create_columns_from_path(imaging_data):
     '''Create a list of subject ids, session ids, modalities, file names and file paths'''
     subject_ids = []
@@ -29,6 +56,7 @@ def create_columns_from_path(imaging_data):
     file_names = []
     file_paths = []
     examinations = []
+    order = []
 
     for f in imaging_data:
         if len(f.parts) >=4:
@@ -37,26 +65,30 @@ def create_columns_from_path(imaging_data):
             file_paths.append(str(f))
             examinations.append(f.name.split('.')[0].split('_')[-1])
             if not '-MR' in f.parts[3]:
-                modalities.append(check_term_in_modality(f.parts[4]))
+                mod = check_term_in_modality(f.parts[4])
+                order.append(make_order(mod))
+                modalities.append(mod)
                 file_names.append(f.parts[5].split('.')[0])
             else:
-                modalities.append(check_term_in_modality(f.name.split('.')[0].split('_')[-1]))
+                mod = check_term_in_modality(f.name.split('.')[0].split('_')[-1])
+                order.append(make_order(mod))
+                modalities.append(mod)
                 file_names.append(f.parts[5].split('.')[0])
 
 
 
-    return subject_ids, session_ids, modalities, file_names, file_paths, examinations
+    return subject_ids, session_ids, modalities, file_names, file_paths, examinations, order
 
 '''Create a dataframe with the columns subject-id, session-id, modality, file-name, file-path'''
-def create_dataframe(subject_ids, session_ids, modalities, file_names, file_paths, examinations):
-    df = pd.DataFrame({'subject-id': subject_ids, 'timepoint': session_ids, 'modality': modalities, 'file-name': file_names, 'file-path': file_paths, 'examintaion': examinations})
+def create_dataframe(subject_ids, session_ids, modalities, file_names, file_paths, examinations, order):
+    df = pd.DataFrame({'subject-id': subject_ids, 'timepoint': session_ids, 'modality': modalities, 'file-name': file_names, 'file-path': file_paths, 'examintaion': examinations, 'order': order})
     return df
 
 '''Run the functions and save the dataframe as a csv file'''
 def main():
     imaging_data = read_imaging_data(data_directory)
-    subject_ids, session_ids, modalities, file_names, file_paths, examinations = create_columns_from_path(imaging_data)
-    df = create_dataframe(subject_ids, session_ids, modalities, file_names, file_paths, examinations)
+    subject_ids, session_ids, modalities, file_names, file_paths, examinations, order = create_columns_from_path(imaging_data)
+    df = create_dataframe(subject_ids, session_ids, modalities, file_names, file_paths, examinations, order)
     df.to_csv('imaging_data.csv', index=False)
 
 '''Get image type from file name'''
